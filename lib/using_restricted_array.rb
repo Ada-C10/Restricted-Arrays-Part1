a87fb9ac0483f9a691ddb17796371ab3512030c4
Laura <larmartinez2@gmail.com>
2018-09-01 17:12:12 -0700

    length method finished

--- lib/using_restricted_array.rb
+++ lib/using_restricted_array.rb
@@ -7,7 +7,12 @@ require_relative 'restricted_array.rb'
 # Calculates the length of the restricted array. All values are integers.
 # The restricted_array is terminated by 'nil' i.e. array[length] = nil
 def length(array)
-  raise NotImplementedError
+  index = 0
+
+  while array[index] != nil
+    index += 1
+  end
+  return index
 end
 
 # Prints each integer values in the array
