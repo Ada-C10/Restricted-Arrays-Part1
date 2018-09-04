f6378adc60ab12f26d19d3a1f4b59c35568b0024
Kat Perreira <kat.perreira@gmail.com>
2018-09-03 20:23:52 -0700

    Refactored and fixed whitespace

--- lib/using_restricted_array.rb
+++ lib/using_restricted_array.rb
@@ -1,5 +1,4 @@
 require_relative 'restricted_array.rb'
-require 'pry'
 # RestrictedArray can be created using a specified size, or a random size in
 # the range of 1-20 will be chosen for you.
 # All values are integers in the range of 1-221.
@@ -26,7 +25,6 @@ def print_array(array)
   return output
 end
 
-#
 # # For an unsorted array, searches for 'value_to_find'.
 # # Returns true if found, false otherwise.
 def search(array, length, value_to_find)
@@ -40,7 +38,7 @@ def search(array, length, value_to_find)
   end
   return false
 end
-#
+
 # Finds and returns the largest integer value the array
 # Assumes that the array is not sorted.
 def find_largest(array, length)
@@ -82,10 +80,9 @@ def reverse(array, length)
   end
   return array
 end
-#
+
 # For an array sorted in ascending order, searches for 'value_to_find'.
 # Returns true if found, false otherwise.
-
 def binary_search(array, length, value_to_find)
   low = 0
   high = length - 1
@@ -105,14 +102,12 @@ def binary_search(array, length, value_to_find)
   end
 end
 
-
 #   # Helper method provided to sort the array in ascending order
 #   # Implements selection sort
 #   # Time complexity = O(n^2) since to find the correct value to be in a given location,
 #   # all the remaining elements are visited. This is done for each location.
 #   # (nested loop of size n each)
-#   # Space complexity = O(1) since the additional storage needed does not depend
-#   #                    on input array size.
+#   # Space complexity = O(1) since the additional storage needed does not depend on input array size.
 def sort(array, length)
   length.times do |index| # outer loop - n elements
     min_index = index # assume index is where the next minimally value is
